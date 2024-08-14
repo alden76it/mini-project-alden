3aba69d9d449c2127d789e6f3cd08d299d16c9ba
alden76it <aldensirait03@gmail.com>
2024-08-14 23:43:36 +0700

    add

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,12 @@
 // start Javascript
 
 console.log("linked success");
+
+function validationFrom() {
+    let input_bb = document.getElementById('input_bb').value;
+    let input_tb = document.getElementById('iinput_tb').value;
+    let converttbTom = input_tb / 100;
+    let inputTbpow = Math.pow(converttbTom.2);
+
+    if input_bb == " "
+}
