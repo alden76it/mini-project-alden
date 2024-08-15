dc25c60f4b761c4c5134d25c35b39c8153d78aab
alden76it <aldensirait03@gmail.com>
2024-08-15 23:59:37 +0700

    add

--- JS/script.js
+++ JS/script.js
@@ -15,6 +15,24 @@ function validationFrom() {
         let inputTbpow = Math.pow(converttbTom, 2);
         let resultBMI = input_bb / inputTbpow;
         console.log(resultBMI)
+
+        document.getElementById("resultBMI").innerText = input_bb / inputTbpow;
+
+        if (resultBMI < 18.5) {
+            document.getElementById("comment").innerHTML = "Kekurangan berat badan";
+        }
+        else if (resultBMI >= 18.5 && resultBMI <= 25) {
+            document.getElementById("comment").innerHTML = "Normal (Ideal)";
+        }
+        else if (resultBMI >= 25 && resultBMI <= 29.9) {
+            document.getElementById("comment").innerHTML = "Kelebihan berat badan";
+        } 
+        else if (resultBMI >= 30) {
+            document.getElementById("comment").innerHTML = "Kegemukan (obesitas)";
+        }
+        return false;
+        
+        let statusBb = document.getElementById("comment").value;
     }
 
 }
