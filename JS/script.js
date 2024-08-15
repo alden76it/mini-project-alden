0c42798c6d73c56b9f055433cc91a35266ef168d
alden76it <aldensirait03@gmail.com>
2024-08-15 21:02:00 +0700

    add

--- JS/script.js
+++ JS/script.js
@@ -4,15 +4,17 @@ console.log("linked success");
 
 function validationFrom() {
     let input_bb = document.getElementById('input_bb').value;
-    let input_tb = document.getElementById('iinput_tb').value;
-    let converttbTom = input_tb / 100;
-    let inputTbpow = Math.pow(converttbTom.2);
-
-    if (input_bb == "") {
-        alert(Data input berat badan kosong)
-    }
-    if (input_tb =="") {
-        alert(Data input tinggi badan kosong)
-    }
+    let input_tb = document.getElementById('input_tb').value;
     
+    if (input_bb == '') {
+        alert('Data input berat badan kosong');
+    } else if (input_tb == '') {
+        alert('Data input tinggi badan kosong');
+     } else {
+        let converttbTom = input_tb / 100;
+        let inputTbpow = Math.pow(converttbTom, 2);
+        let resultBMI = input_bb / inputTbpow;
+        console.log(resultBMI)
+    }
+
 }
